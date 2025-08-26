616d02ebb5982c1978b093add8ec465248c59403
ffilippopoulos <ffilippopoulos@uw.co.uk>
2025-08-26 15:20:47 +0100

    Deploy iptables firewall rules on etcd instances

--- common.tf
+++ common.tf
@@ -16,3 +16,19 @@ data "ignition_disk" "devsda" {
     number = 1
   }
 }
+
+data "ignition_systemd_unit" "iptables-rule-load" {
+  name = "iptables-rule-load.service"
+
+  content = <<EOS
+[Unit]
+Description=Loads presaved iptables rules from /var/lib/iptables/rules-save
+[Service]
+Type=oneshot
+ExecStart=/usr/sbin/iptables-restore /var/lib/iptables/rules-save
+[Install]
+WantedBy=multi-user.target
+EOS
+
+}
+
